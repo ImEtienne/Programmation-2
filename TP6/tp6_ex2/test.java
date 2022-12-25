44639833070333a71d1bc37ea0bbdda84de3cd10
Etienne KIBONGANI LOMBO <86003446+ImEtienne@users.noreply.github.com>
2022-12-25 20:33:23 +0100

    commit3

---
+++ TP6/tp6_ex2/test.java
@@ -0,0 +1,9 @@
+public class test{
+    public static void main(String args[]){
+        int[][] t = new int[10][30];
+
+        for(int i = 0; i<t[0].length; i++){
+            System.out.println(i);
+        }
+    }
+}
